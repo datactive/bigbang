99698d323280d83165b34a1be2bed677efc8fdcf
sb <sb@ischool.berkeley.edu>
2014-06-19 13:53:35 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Analysis of Mailman archives',
+    'author': 'Sebastian Benthall',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'sb@ischool.berkeley.edu',
+    'version': '0.1',
+    'install_requires': ['nose,networkx'],
+    'packages': ['bigbang'],
+    'scripts': [],
+    'name': 'BigBang'
+}
+
+setup(**config)
