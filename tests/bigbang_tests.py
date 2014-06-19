aff45b7618b5abd7c15789df2df98c887d70f557
sb <sb@ischool.berkeley.edu>
2014-06-19 14:21:42 -0700

    adding test fixture .txt message

--- tests/bigbang_tests.py
+++ tests/bigbang_tests.py
@@ -1,11 +1,27 @@
 from nose.tools import *
 import bigbang
+import os
+
+test_txt = ""
 
 def setup():
-    print "SETUP!"
+    # load a sample txt file
+    dirname = os.path.dirname(os.path.abspath(__file__))
+
+    test_file = open(os.path.join(dirname,"2001-November.txt"))
+
+    global test_txt
+    test_txt = test_file.read()
 
 def teardown():
     print "TEAR DOWN!"
 
+@with_setup(setup, teardown)
 def test_basic():
+
+    print test_txt
+    if len(test_txt) > 0:
+        pass
+    else:
+        assert False
     print "I RAN!"
