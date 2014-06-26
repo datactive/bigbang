ccd7f76a461a53f1c20906307b7a5647853da0d5
sb <sb@ischool.berkeley.edu>
2014-06-26 10:05:50 -0700

    use pydot for visualization in tograph method

--- bin/tograph.py
+++ bin/tograph.py
@@ -13,9 +13,15 @@ RG = graph.messages_to_reply_graph(messages)
 
 IG = graph.messages_to_interaction_graph(messages)
 
+pdig = nx.to_pydot(IG)
+
+pdig.set_overlap('False')
+
+pdig.write_png('interactions.png',prog='neato')
+
 #print nx.connected_components(G.to_undirected())
 
-nx.write_dot(IG,"interactions.dot")
+#nx.write_dot(IG,"interactions.dot")
 
 #nx.draw_shell(G)
 #plt.show()
