0403a961ee6a6a5829c083d1b3e8e6934fb08e22
Spiros Eliopoulos <seliopou@gmail.com>
2014-07-01 21:45:59 -0400

    parser: organize compiled regular expressions
    Put them in a dictionary and look them up when you need them. Slower but
    no matter right now.

--- bigbang/parse.py
+++ bigbang/parse.py
@@ -3,23 +3,24 @@ import email
 import re
 import dateutil.parser as dparse
 
+
+re_cache = {
+  'top_exp'     : re.compile("From .*\d\d\d\d\n"),
+  'msg_id'      : re.compile("<\S*@\S*>"),
+  'msg_from'    : re.compile("\(([^()]+)\)")
+}
+
 def open_mail_archive(filename):
     f = open(filename)
 
-    top_exp = re.compile("From .*\d\d\d\d\n")
-
-    mails = top_exp.split(f.read())
+    mails = re_cache['top_exp'].split(f.read())
 
     messages = [email.message_from_string(m) for m in mails if m is not '']
 
     return messages
 
-msg_id_re = re.compile("<\S*@\S*>")
-
 def split_references(refs):
     return msg_id_re.findall(refs)
-
-msg_id_re = re.compile("<\S*@\S*>")
 
 def get_refs(refs):
     return msg_id_re.findall(refs)
@@ -27,7 +28,5 @@ def get_refs(refs):
 def clean_mid(mid):
     return get_refs(mid)[0]
 
-msg_from_re = re.compile("\(([^()]+)\)")
-
 def clean_from(m_from):
-    return msg_from_re.findall(m_from)[0]
+    return re_cache['msg_from'].findall(m_from)[0]
