be439447ed4f0db5c22ca2023735a30ca5de1779
sb <sbenthall@gmail.com>
2014-07-02 18:19:48 -0700

    compute ascendancy and capacity over time for a list. closes #30

--- bin/plot_activity.py
+++ bin/plot_activity.py
@@ -1,11 +1,12 @@
 import bigbang.mailman as mailman
+import bigbang.graph as graph
 from bigbang.parse import get_date
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 import pytz
-
 
 url = "http://mail.scipy.org/pipermail/scipy-dev/"
 
@@ -82,6 +83,51 @@ for values in data.values:
 total_activity = np.sum(activity,0)
 participant_activity = np.sum(activity > 0,0)
 
-plt.plot(smooth(total_activity,30),'r')
-plt.plot(smooth(participant_activity,30),'b')
+plt.plot(smooth(total_activity,100),'r')
+plt.plot(smooth(participant_activity,100),'b')
+plt.show()
+
+IG = graph.messages_to_interaction_graph(messages)
+matrix = graph.interaction_graph_to_matrix(IG)
+
+# removing last email in case it was sent in 2083.
+# don't ask
+#sorted_messages = sorted(messages,key=get_date)[:-1]
+
+dated_messages = {}
+
+for m in messages:
+    o = get_date(m).toordinal()
+
+    dated_messages[o] = dated_messages.get(o,[])
+    dated_messages[o].append(m)
+
+
+epoch = max(days)-min(days)
+
+step = 1
+duration = 100
+
+ascendancy = np.zeros([max(days)-min(days)+1])
+capacity = np.zeros(([max(days)-min(days)+1]))
+
+
+for i in range(epoch/step):
+    min_d = min(days) + i * step
+    max_d = min_d + duration
+
+    block_messages = []
+
+    for d in range(min_d,max_d):
+        block_messages.extend(dated_messages.get(d,[]))
+
+    b_IG = graph.messages_to_interaction_graph(block_messages)
+    b_matrix = graph.interaction_graph_to_matrix(b_IG)
+
+    ascendancy[min_d-day_offset] = graph.ascendancy(b_matrix)
+    capacity[min_d-day_offset] = graph.capacity(b_matrix)
+
+plt.plot(ascendancy,'o')
+plt.plot(capacity,'y')
+
 plt.show()
