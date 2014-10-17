b02357e28f82dae4681131a9ced5b51378573c00
Dave Lester <davelester@gmail.com>
2014-10-17 00:31:33 -0700

    Updates bigbang tests to be pep8 compliant.

--- tests/bigbang_tests.py
+++ tests/bigbang_tests.py
@@ -7,27 +7,32 @@ import os
 
 test_txt = ""
 
+
 def setup():
     pass
-    
+
+
 def teardown():
     pass
+
 
 @with_setup(setup, teardown)
 def test_parse():
 
     dirname = os.path.dirname(os.path.abspath(__file__))
-    test_file_name = os.path.join(dirname,"2001-November.txt")
+    test_file_name = os.path.join(dirname, "2001-November.txt")
 
-    messages = mailbox.mbox(test_file_name,create=False).values()
+    messages = mailbox.mbox(test_file_name, create=False).values()
 
     for message in messages:
         assert len(message.items()) <= 6, "too many fields:\n%s" % (message)
+
 
 def test_split_references():
     refs = " <ye1y9ljtxwk.fsf@orange30.ex.ac.uk>\n\t<055701c16727$b57fed90$8fd6afcf@pixi.com>"
     split = parse.split_references(refs)
     assert len(split) == 2, split
+
 
 def test_mailman_chain():
     url = "http://mail.scipy.org/pipermail/ipython-dev/"
