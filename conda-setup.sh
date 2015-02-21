342c24d72acb1ae7b3b3445d50c90f6fe7a4c65b
sb <sbenthall@gmail.com>
2015-02-21 11:08:00 -0800

    adding chardet and html2text to conda-setup

--- conda-setup.sh
+++ conda-setup.sh
@@ -1,5 +1,7 @@
 conda install -n bigbang -c https://conda.binstar.org/asmeurer --file conda-requirements.txt
 source activate bigbang
+pip install chardet
+pip install html2text
 pip install python-Levenshtein
 pip install git+https://github.com/gitpython-developers/GitPython.git
 pip install -e .
