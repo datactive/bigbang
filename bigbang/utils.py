295a73c98d0e39c3f827d1aa0b9bee78b45bbfd6
sb <sbenthall@gmail.com>
2015-02-26 15:24:02 -0800

    section off removing quoted text from an email into its own function

--- bigbang/utils.py
+++ bigbang/utils.py
@@ -1,7 +1,4 @@
-## remove this when clean_message is added to generic libraries
-def clean_message(mess):
-    if mess is None:
-        mess = ''
+def remove_quoted(mess):
     mess.split('\n')
     message = list()
     for l in mess.split('\n'):
@@ -12,4 +9,13 @@ def clean_message(mess):
     for l in message:
         new = new + l + '\n'
     return new
+
+## remove this when clean_message is added to generic libraries
+def clean_message(mess):
+    if mess is None:
+        mess = ''
+    
+    mess = remove_quoted(mess)
+
+    return mess
 
