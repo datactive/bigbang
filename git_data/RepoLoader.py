974c7a2e1765365dd40705e6ae7b41d9f984a118
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-03-17 13:27:58 -0700

    Small bug with repo loader

--- git_data/RepoLoader.py
+++ git_data/RepoLoader.py
@@ -10,7 +10,6 @@ repoLocation = os.path.dirname(os.path.realpath(__file__))
 print(repoLocation);
 last_index = repoLocation.rfind("/")
 repoLocation = repoLocation[0:last_index] + "/git_data/sample_git_repos/"
-repoLocation = "../git_data/sample_git_repos/";
 nameRegex = re.compile("([a-z]*)(\\.git$)")
 
 
