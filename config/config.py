ede8f4ac6f324026c2e85c77018d7004023787e7
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-04-20 14:56:23 -0700

    leaving this branch baye

--- config/config.py
+++ config/config.py
@@ -1,12 +1,26 @@
 import yaml
 import os
 base_loc = os.path.dirname(os.path.realpath(__file__))
-last_index = repoLocation.rfind("/")
+last_index = base_loc.rfind("/")
 base_loc = base_loc[0:last_index] + "/"
+print(base_loc)
 
-stream = open("config.yml", "r")
-docs = yaml.load_all(stream)
-for doc in docs:
-    for k,v in doc.items():
-        print k, "->", v
-    print "\n",
+config_filepath = base_loc + "config/config.yml"
+print(config_filepath);
+stream = open(config_filepath, "r")
+dictionary = yaml.load(stream)
+
+class Config(object):
+	def __init__(self, conf):
+		self.CONFIG = conf;
+
+	def __getattr__(self, query):
+		if query in self.CONFIG:
+			ans = self.CONFIG[query];
+			if "path" in query:
+				ans = base_loc + ans
+			return ans
+		else:
+			return None
+
+CONFIG = Config(dictionary)
