5746543d04568f40c27746d92c34cb30e3cd149d
sb <sbenthall@gmail.com>
2015-05-15 12:15:42 -0700

    partition cleaning in plot.stack

--- bigbang/plot.py
+++ bigbang/plot.py
@@ -10,13 +10,26 @@ def stack(df,partition=None,smooth=1,figsize=(12.5, 7.5),time=True):
     partition - a list of lists of columns of df
     smooth - an integer amount of convolution
     """
+
+    # partitioning logic
     if partition is None:
         #every column stacked separately
         partition = [[c] for c in df.columns]
 
+    
+    # if an item in the portition is not in the dataframe,
+    # clean it up
+    partition = [[i for i in p if i in df.columns]
+                 for p in partition]
+    # remove empty partitions
+    partition = [p for p in partition if p]
+
+    #convolution logic
     convolve_array = np.array([1. / smooth] * smooth)
 
-    part_d = [np.convolve(df.as_matrix(part).sum(1),convolve_array,mode='same')
+    part_d = [np.convolve(df.as_matrix(part).sum(1),
+                          convolve_array,
+                          mode='same')
                  for part in partition]
 
     d = np.row_stack(part_d)
@@ -25,6 +38,7 @@ def stack(df,partition=None,smooth=1,figsize=(12.5, 7.5),time=True):
 
     plt.stackplot(df.index,d,linewidth=0)
 
+    # time axes
     if time:
         fig.axes[0].xaxis_date()
 
