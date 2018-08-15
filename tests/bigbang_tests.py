cb4251942a416fbc8cb6dbbf384f83211d655fc5
Nick Doty <npdoty@ischool.berkeley.edu>
2018-08-15 12:53:55 -0700

    temporary directory instead the tests data folder, with setup and takedown
    tests for provenance population, access and update

--- tests/bigbang_tests.py
+++ tests/bigbang_tests.py
@@ -14,17 +14,23 @@ import pandas as pd
 from config.config import CONFIG
 
 test_txt = ""
+TEMP_DIR = os.path.join(CONFIG.test_data_path, "tmp")
 
 def test_git_dependancy():
     repo = repo_loader.get_repo("https://github.com/sbenthall/bigbang.git", in_type = "remote")
 
 def setup():
-    pass
+    try:
+        os.mkdir(TEMP_DIR)
+    except OSError: # Python 2.7-specific, alas; FileExistsError in py3
+        pass # temporary directory already exists, that's cool
 
 
 def teardown():
-    pass
-
+    # remove all files in the temporary files directory, as cleanup
+    temp_files = os.listdir(TEMP_DIR)
+    for f in temp_files:
+        os.remove(os.path.join(TEMP_DIR, f))
 
 def test_split_references():
     refs = " <ye1y9ljtxwk.fsf@orange30.ex.ac.uk>\n\t<055701c16727$b57fed90$8fd6afcf@pixi.com>"
@@ -170,3 +176,22 @@ def test_activity_summary():
 
     assert str(type(activity_frame)) == "<class 'pandas.core.frame.DataFrame'>", "not a DataFrame?"
     assert len(activity_frame.columns) == 1, "activity summary should have one column"
+
+def test_provenance():
+    test_list_name = 'test-list-name'
+    test_list_url = 'https://example.com/test-list-url/'
+    test_notes = 'Test notes.'
+    mailman.populate_provenance(TEMP_DIR, list_name=test_list_name, list_url=test_list_url, notes=test_notes)
+
+    assert os.path.exists(os.path.join(TEMP_DIR, mailman.PROVENANCE_FILENAME)), "provenance file should have been created"
+
+    provenance = mailman.access_provenance(TEMP_DIR)
+    assert provenance != None, "provenance should be something"
+    assert provenance['list']['list_name'] == test_list_name, "list name should be in the provenance"
+    assert provenance['list']['list_url'] == test_list_url, "list url should be in the provenance"
+    assert provenance['notes'] == test_notes, "notes should be in the provenance"
+
+    provenance['notes'] = 'modified provenance'
+    mailman.update_provenance(TEMP_DIR, provenance)
+    provenance_next = mailman.access_provenance(TEMP_DIR)
+    assert provenance_next['notes'] == 'modified provenance', "confirm modified provenance"
