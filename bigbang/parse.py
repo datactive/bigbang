a7545f47a124963f397817e83e368ada45edd1ae
Nick Doty <npdoty@ischool.berkeley.edu>
2019-02-05 17:25:46 -0800

    new parse functions for handling names and email addresses, cleaned and tokenized

--- bigbang/parse.py
+++ bigbang/parse.py
@@ -48,6 +48,65 @@ def clean_from(m_from):
 
     return cleaned
 
+def normalize_email_address(address):
+    """
+    Takes a valid email address and returns a normalized one, for matching purposes.
+    """
+    # TODO: drop the + labeling
+    return address.lower()
+
+def clean_name(name):
+    """
+    Clean just the name portion from email.utils.parseaddr.
+
+    Returns None if the name portion is missing anything name-like. Otherwise, returns the cleaned name.
+    """
+
+    # we see these specific strings due to parsing issues in email somewhere
+    name = name.replace('unknown charset', ' ') 
+    name = name.replace('wrong string', ' ')
+    name = name.replace('_', ' ')
+
+    # these are stop characters we can just delete
+    stop_characters = unicode('"<>\\()/:?%!+\'@')
+    stop_characters_map = dict((ord(char), None) for char in stop_characters)
+
+    name = unicode(name, 'utf-8', 'ignore').translate(stop_characters_map)
+
+    # do we need to also catch email archives that use anti-spam measures?
+    # like: .replace(' at ','@')
+
+    # TODO: decode or collapse rfc2231 encodings, like '=?utf-8?q?carlos_gonz=c3=a1lez-cadenas?=' ?
+
+    name = name.strip() # remove leading and trailing whitespace
+
+    if len(name) > 0:
+        return name
+    else:
+        return None
+
+def tokenize_name(clean_name):
+    """
+    Create a tokenized version of a name, good for comparison and sorting for entity resolution.
+
+    Takes a Unicode name already cleaned of most punctuation and spurious characters, hopefully.
+    """
+
+    # make lower case, remove "." and ",", tokenize and lexicographically sort the tokens, join by spaces, return as a string
+
+    stop_characters = unicode('".,')
+    stop_characters_map = dict((ord(char), None) for char in stop_characters)
+    name = clean_name.translate(stop_characters_map)
+
+    tokens = name.lower().split() # splits on whitespace
+    
+    if len(tokens) == 0:
+        return None
+
+    tokenized_name = ' '.join(sorted(tokens)) 
+    
+    return tokenized_name
+
 def guess_first_name(cleaned_from):
     """
     Attempt to extract a person's first name from the cleaned version of their name
