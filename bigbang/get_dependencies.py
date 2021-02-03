da93ffd875169770a2e90ef7c616e785e372ae28
Christovis <chrbecker01@gmail.com>
2021-02-03 10:07:54 +0000

    formatting get_dependencies.py

--- bigbang/get_dependencies.py
+++ bigbang/get_dependencies.py
@@ -1,10 +1,15 @@
-import warnings
-warnings.simplefilter("ignore", UserWarning)
 import sys
+import warnings
+
 from snakefood import gendeps
+
+warnings.simplefilter("ignore", UserWarning)
 sys.stdout = open("dependencies.txt", "w")
 
-def main():
-	gendeps.gendeps()
 
-if __name__ == "__main__": main()
+def main():
+    gendeps.gendeps()
+
+
+if __name__ == "__main__":
+    main()
