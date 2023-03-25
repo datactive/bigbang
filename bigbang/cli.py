07dbc17bed4a0021fcc8a45b41f667be2b83b19e
Micah Lee <micah@micahflee.com>
2023-03-25 14:47:01 +0900

    Implement CLI collect-mail subcommand

--- bigbang/cli.py
+++ bigbang/cli.py
@@ -1,6 +1,22 @@
 import click
+from .ingress import mailman
 
-@click.command()
+
+@click.group()
 def main():
     """BigBang CLI tools"""
-    pass
+
+
+@main.command()
+@click.option("--url", 'url', help="URL of mailman archive")
+@click.option("--file", 'file', help="Path of a file with linebreak-seperated list of URLs")
+@click.option("--archives", 'archives', default="./archives", show_default=True, help="Path to a specified directory for storing downloaded mail archives")
+@click.option("--notes", 'notes', help="Notes to record regarding provenance")
+def collect_mail(url, file, archives, notes):
+    """Collects files from public mailing list archives"""
+    if url:
+        mailman.collect_from_url(url, archive_dir=archives, notes=notes)
+    elif file:
+        mailman.collect_from_file(file, archive_dir=archives, notes=notes)
+    else:
+        print("Either --url or --file must be set")
